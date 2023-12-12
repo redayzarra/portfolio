fd32da1c2a6d22ce6ce43887558aa81d171cf3f3
ReDay Zarra <redayzarra@gmail.com>
2023-12-11 20:41:16 -0500

    Adding light and dark mode to my portfolio

---
+++ components/ModeToggle.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { RxMoon } from "react-icons/rx";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <Button variant="outline" size="sm" onClick={toggleTheme}>
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <RxMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
