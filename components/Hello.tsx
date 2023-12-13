ed94d54d6afa77a22846d216d3324e143bc3c451
ReDay Zarra <redayzarra@gmail.com>
2023-12-12 23:39:54 -0500

    I'm liking what I'm seeing in the hello component

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React from "react";
 import Steve from "@/public/images/SteveRodgers.jpg";
 import { Button } from "./ui/button";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
 
 const Hello = ({ className }: { className?: string }) => {
   return (
@@ -18,13 +20,17 @@ const Hello = ({ className }: { className?: string }) => {
           />
         </div>
         {/* Greeting */}
-        <div className="flex flex-col items-center justify-center space-y-2">
-          <p>Hello! I'm</p>
-          <p className="text-3xl font-black">ReDay Zarra</p>
-          <p className="text-muted-foreground">Software Engineer</p>
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-sm text-muted-foreground">Hello! I'm</p>
+            <p className="mt-1 text-3xl font-bold">ReDay Zarra</p>
+            <p className="mt-2 text-xl font-medium text-muted-foreground">
+              Software Engineer
+            </p>
+          </div>
 
           {/* Resume and Contact Buttons */}
-          <div className="space-x-2">
+          <div className="mt-4 space-x-2">
             <Button
               className="rounded-full border-[2px] border-foreground"
               variant="outline"
@@ -32,6 +38,14 @@ const Hello = ({ className }: { className?: string }) => {
               Resume
             </Button>
             <Button className="rounded-full">Contact</Button>
+          </div>
+          <div className="mt-2 flex items-center justify-center space-x-2">
+            <Link href="https://www.linkedin.com/in/redayzarra/">
+              <FaLinkedin size={30} className="rounded-full fill object-fill" />
+            </Link>
+            <Link href="https://github.com/redayzarra">
+              <FaGithub size={30} />
+            </Link>
           </div>
         </div>
       </div>
