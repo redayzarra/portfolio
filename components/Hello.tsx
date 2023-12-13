db23e252b644ba2e9a241c748800d208d89a6e6e
ReDay Zarra <redayzarra@gmail.com>
2023-12-12 23:23:48 -0500

    Finishing up Hello component

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import React from "react";
 import Steve from "@/public/images/SteveRodgers.jpg";
+import { Button } from "./ui/button";
 
 const Hello = ({ className }: { className?: string }) => {
   return (
     <div className={`flex items-center justify-center ${className}`}>
       <div className="flex space-x-10">
         {/* My photo */}
-        <div className="relative h-32 w-32 rounded-full overflow-hidden">
+        <div className="relative h-48 w-48 rounded-full overflow-hidden">
           <Image
             src={Steve}
             alt="My Picture"
@@ -16,10 +17,22 @@ const Hello = ({ className }: { className?: string }) => {
             className="rounded-full"
           />
         </div>
-        {/* My introduction */}
-        <div className="flex flex-col items-center justify-center">
+        {/* Greeting */}
+        <div className="flex flex-col items-center justify-center space-y-2">
           <p>Hello! I'm</p>
           <p className="text-3xl font-black">ReDay Zarra</p>
+          <p className="text-muted-foreground">Software Engineer</p>
+
+          {/* Resume and Contact Buttons */}
+          <div className="space-x-2">
+            <Button
+              className="rounded-full border-[2px] border-foreground"
+              variant="outline"
+            >
+              Resume
+            </Button>
+            <Button className="rounded-full">Contact</Button>
+          </div>
         </div>
       </div>
     </div>
