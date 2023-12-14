f20eba297b4fce069baea290a57c6c2e9e9da414
ReDay Zarra <redayzarra@gmail.com>
2023-12-13 21:38:28 -0500

    Animating the menu to open and close - bug present

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -2,14 +2,25 @@
 
 import React, { useState } from "react";
 import MenuButton from "./MenuButton";
+import { motion } from "framer-motion";
+
+const variants = {
+  open: { width: 480, height: 650 },
+  closed: { width: 80, height: 40 },
+};
 
 const Menu = () => {
   const [isActive, setIsActive] = useState(false);
   return (
     <div className="relative">
-      <div className="menu relative">
+      <motion.div
+        className="menu relative"
+        variants={variants}
+        animate={isActive ? "open" : "closed"}
+        initial="closed"
+      >
         <MenuButton isActive={isActive} setIsActive={setIsActive} />
-      </div>
+      </motion.div>
     </div>
   );
 };
