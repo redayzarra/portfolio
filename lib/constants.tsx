26e5c641bb8eada6e286fcc76022bb31d469d41c
ReDay Zarra <redayzarra@gmail.com>
2023-12-17 18:36:07 -0500

    Making nicer looking skills section

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -1,8 +1,15 @@
-import { BiLogoJavascript, BiLogoTypescript } from "react-icons/bi";
+import {
+  BiLogoGoogleCloud,
+  BiLogoJavascript,
+  BiLogoTypescript,
+} from "react-icons/bi";
 import { CgCPlusPlus } from "react-icons/cg";
 import {
+  FaAngular,
   FaAws,
   FaCss3,
+  FaDocker,
+  FaGitAlt,
   FaHtml5,
   FaNodeJs,
   FaPython,
@@ -15,6 +22,7 @@ import {
   SiAxios,
   SiExpress,
   SiFlask,
+  SiGooglecloud,
   SiJest,
   SiKeras,
   SiNextdotjs,
@@ -28,42 +36,51 @@ import {
 import { TbBrandThreejs } from "react-icons/tb";
 import { FcHeatMap } from "react-icons/fc";
 import { SlGraph } from "react-icons/sl";
+import { FaUnity } from "react-icons/fa";
+
+const iconSize = 75;
 
 export const frontendSkills = [
-  { icon: <FaHtml5 size={80} />, name: "HTML" },
-  { icon: <FaCss3 size={80} />, name: "CSS" },
-  { icon: <SiTailwindcss size={80} />, name: "Tailwind" },
-  { icon: <BiLogoJavascript size={80} />, name: "Javascript" },
-  { icon: <BiLogoTypescript size={80} />, name: "Typescript" },
-  { icon: <FaReact size={80} />, name: "React" },
-  { icon: <SiNextdotjs size={80} />, name: "Next.js" },
-  { icon: <SiJest size={80} />, name: "Jest" },
-  { icon: <FiFramer size={80} />, name: "Framer Motion" },
-  { icon: <TbBrandThreejs size={80} />, name: "Three.js" },
+  { icon: <FaHtml5 size={iconSize} />, name: "HTML" },
+  { icon: <FaCss3 size={iconSize} />, name: "CSS" },
+  { icon: <BiLogoJavascript size={iconSize} />, name: "Javascript" },
+  { icon: <BiLogoTypescript size={iconSize} />, name: "Typescript" },
+  { icon: <FaReact size={iconSize} />, name: "React" },
+  { icon: <SiNextdotjs size={iconSize} />, name: "Next.js" },
+  { icon: <FaAngular size={iconSize} />, name: "Angular" },
+  { icon: <SiJest size={iconSize} />, name: "Jest" },
+  { icon: <TbBrandThreejs size={iconSize} />, name: "Three.js" },
+  { icon: <FiFramer size={iconSize} />, name: "Framer Motion" },
+  { icon: <SiTailwindcss size={iconSize} />, name: "Tailwind" },
+  { icon: <FaGitAlt size={iconSize} />, name: "Git" },
 ];
 
 export const backendSkills = [
-  { icon: <FaPython size={80} />, name: "Python" },
-  { icon: <BiLogoTypescript size={80} />, name: "Typescript" },
-  { icon: <CgCPlusPlus size={80} />, name: "C++" },
-  { icon: <GrMysql size={80} />, name: "MySQL" },
-  { icon: <SiPostgresql size={80} />, name: "PostgresSQL" },
-  { icon: <FaAws size={80} />, name: "AWS" },
-  { icon: <SiAxios size={80} />, name: "Axios" },
-  { icon: <SiFlask size={80} />, name: "Flask" },
-  { icon: <FaNodeJs size={80} />, name: "Node.js" },
-  { icon: <SiExpress size={80} />, name: "Express" },
+  { icon: <FaPython size={iconSize} />, name: "Python" },
+  { icon: <BiLogoTypescript size={iconSize} />, name: "Typescript" },
+  { icon: <SiAxios size={iconSize} />, name: "Axios" },
+  { icon: <CgCPlusPlus size={iconSize} />, name: "C++" },
+  { icon: <GrMysql size={iconSize} />, name: "MySQL" },
+  { icon: <SiPostgresql size={iconSize} />, name: "PostgreSQL" },
+  { icon: <FaNodeJs size={iconSize} />, name: "Node.js" },
+  { icon: <SiExpress size={iconSize} />, name: "Express" },
+  { icon: <SiFlask size={iconSize} />, name: "Flask" },
+  { icon: <FaDocker size={iconSize} />, name: "Docker" },
+  { icon: <FaAws size={iconSize} />, name: "AWS" },
+  { icon: <BiLogoGoogleCloud size={iconSize} />, name: "Google Cloud" },
 ];
 
 export const machineLearningSkills = [
-  { icon: <FaPython size={80} />, name: "Python" },
-  { icon: <CgCPlusPlus size={80} />, name: "C++" },
-  { icon: <SiTensorflow size={80} />, name: "TensorFlow" },
-  { icon: <SiPytorch size={80} />, name: "PyTorch" },
-  { icon: <SiScikitlearn size={80} />, name: "Scikit-Learn" },
-  { icon: <SiKeras size={80} />, name: "Keras" },
-  { icon: <SlGraph size={80} />, name: "Matplotlib" },
-  { icon: <SiPandas size={80} />, name: "Pandas" },
-  { icon: <FaRProject size={80} />, name: "R (langugage)" },
-  { icon: <FaAws size={80} />, name: "AWS" },
+  { icon: <FaPython size={iconSize} />, name: "Python" },
+  { icon: <CgCPlusPlus size={iconSize} />, name: "C++" },
+  { icon: <SiTensorflow size={iconSize} />, name: "TensorFlow" },
+  { icon: <SiPytorch size={iconSize} />, name: "PyTorch" },
+  { icon: <SiScikitlearn size={iconSize} />, name: "Scikit-Learn" },
+  { icon: <SiKeras size={iconSize} />, name: "Keras" },
+  { icon: <SlGraph size={iconSize} />, name: "Matplotlib" },
+  { icon: <SiPandas size={iconSize} />, name: "Pandas" },
+  { icon: <FaRProject size={iconSize} />, name: "R (langugage)" },
+  { icon: <FaAws size={iconSize} />, name: "AWS" },
+  { icon: <BiLogoGoogleCloud size={iconSize} />, name: "Google Cloud" },
+  { icon: <FaUnity size={iconSize} />, name: "Unity (RL)" },
 ];
