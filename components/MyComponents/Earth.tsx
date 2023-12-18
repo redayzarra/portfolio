e35eabfcdee3e63655d2967d9e821fd6450df57a
ReDay Zarra <redayzarra@gmail.com>
2023-12-17 21:31:49 -0500

    Working on making the Earth more interesting

--- components/MyComponents/Earth.tsx
+++ components/MyComponents/Earth.tsx
@@ -1,18 +1,26 @@
 "use client";
 
-import { Canvas, useLoader, useFrame } from "@react-three/fiber";
-import { useRef } from "react";
-import { TextureLoader, Mesh } from "three";
+import { Canvas, ThreeEvent, useFrame, useLoader } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { Mesh, TextureLoader } from "three";
 
 const EarthMesh = () => {
   const meshRef = useRef<Mesh>();
-
+  const [isHovering, setIsHovering] = useState(false); // New state to track cursor hovering
+  const [rotation, setRotation] = useState({ x: 0, y: 0 });
   const animationSpeed = 0.001;
 
   useFrame(() => {
     if (meshRef.current) {
-      meshRef.current.rotation.y += animationSpeed; // Speed
-      meshRef.current.rotation.x += animationSpeed;
+      if (isHovering) {
+        // Apply cursor-based rotation when hovering
+        meshRef.current.rotation.x += rotation.x;
+        meshRef.current.rotation.y += rotation.y;
+      } else {
+        // Apply default rotation when not hovering
+        meshRef.current.rotation.y += animationSpeed;
+        meshRef.current.rotation.x += animationSpeed;
+      }
     }
   });
 
@@ -22,8 +30,28 @@ const EarthMesh = () => {
     "/images/Earth/occlusion.webp",
   ]);
 
+  const handlePointerEnter = () => setIsHovering(true); // Set hovering to true
+  const handlePointerLeave = () => setIsHovering(false); // Set hovering to false
+
+  const handleMouseMove = (event: ThreeEvent<PointerEvent>) => {
+    const { clientX, clientY } = event;
+    const mouseX = clientX;
+    const mouseY = clientY;
+
+    setRotation({
+      x: mouseY / 100_100,
+      y: mouseX / 100_100,
+    });
+  };
+
   return (
-    <mesh ref={meshRef as any} scale={3}>
+    <mesh
+      ref={meshRef as any}
+      scale={3}
+      onPointerMove={handleMouseMove}
+      onPointerEnter={handlePointerEnter}
+      onPointerLeave={handlePointerLeave}
+    >
       <sphereGeometry args={[1, 100, 100]} />
       <meshStandardMaterial map={color} normalMap={normal} aoMap={aoMap} />
     </mesh>
