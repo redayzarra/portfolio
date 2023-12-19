a621109af6339eb808730d66bd1f7190708a74b3
ReDay Zarra <redayzarra@gmail.com>
2023-12-19 03:46:55 -0500

    Adding space for active projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -17,7 +17,25 @@ interface Props {
 const Projects = ({ projects }: Props) => {
   return (
     <div className="flex items-center justify-center">
-      <div className="grid gap-14 grid-cols-1 md:grid-cols-2 items-center justify-center">
+      <div className="grid w-auto gap-10 grid-cols-1 md:grid-cols-2 items-center justify-center">
+        <ProjectCard
+          title="StudyDSA"
+          label="In progress"
+          githubUrl="https://github.com/redayzarra/StudyDSA.com"
+          image=""
+          demoUrl=""
+          videoUrl=""
+        />
+        <div className="flex flex-col w-80 space-y-3">
+          <h1 className="text-xl font-semibold mt-4 md:mt-0 flex items-center justify-center md:block">
+            Hands-On Learning
+          </h1>
+          <p className="text-muted-foreground">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            quaerat fugit quod minima distinctio ratione possimus impedit. Quam,
+            fugiat. Atque.
+          </p>
+        </div>
         {projects.map((project) => {
           const { title, label, githubUrl, image, demoUrl, videoUrl } = project;
           return (
